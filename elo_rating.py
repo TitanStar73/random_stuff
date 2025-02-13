fb91ae052cd3ef5e296a1f976c99f3e63bca1b42
TitanStar73 <153201417+TitanStar73@users.noreply.github.com>
2025-02-13 17:13:57 +0530

    Update elo_rating.py

--- elo_rating.py
+++ elo_rating.py
@@ -1,3 +1,7 @@
 #Simple elo rating implementation for game rating
 
-def calculate_rating(p1_rating, p2_rating, p1_outcome):
+def calculate_rating(p1_rating, p2_rating, p1_outcome, k=40):
+    """p1_outcome: 0 to 1, determining how much p1 won 1=won, 0=loss; k=max change in score"""
+    ea = 1/(1+10**((p2_rating-p1_rating)/400))
+
+    
